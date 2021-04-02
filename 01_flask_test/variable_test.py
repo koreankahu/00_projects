4c21d61f33b11fa31f841565d17a859be6889403
daeil <ceoclub00@naver.com>
2021-04-03 00:23:06 +0900

    'janja2'

--- 01_flask_test/variable_test.py
+++ 01_flask_test/variable_test.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/hello/<user>')
 def hello_name(user):
-    return render_template('variable.html', name1=user, name2=2)
+    return render_template('variable.html', name1=user, name2="dd")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port='8081')
