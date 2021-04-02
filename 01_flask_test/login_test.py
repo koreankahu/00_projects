e7887e5665e303652df472f7b3a3ec9b3b0e5283
daeil <ceoclub00@naver.com>
2021-04-02 23:04:15 +0900

    03 flask bootstrap

--- 01_flask_test/login_test.py
+++ 01_flask_test/login_test.py
@@ -7,8 +7,8 @@ def login():
     username = request.args.get('user_name')
     passwd = request.args.get('pw')
     email = request.args.get('email_address')
-    print (username, passwd, email)
-    
+    print(username, passwd, email)
+
     if username == 'dave':
         return_data = {'auth': 'success'}
     else:
@@ -18,9 +18,9 @@ def login():
 
 @app.route('/html_test')
 def hello_html():
-    # html file은 templates 폴더에 위치해야 함
+    # html file은 templates 폴더에 위치해야함
     return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="127.0.0.1", port="8081")
